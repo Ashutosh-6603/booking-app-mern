d2410460d7755aff1c2920cfc2e01355d33606bc
Ashutosh-Patro <ashutosh@itobuz.com>
2024-01-31 01:51:00 +0530

    Created endpoints for user registration and login resume:1:32

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,16 +1,14 @@
 import express, { Request, Response } from "express";
-import User from "../models/user";
-import jwt from "jsonwebtoken";
 import { check, validationResult } from "express-validator";
+import User from "../models/user";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-// /api/users/register
 router.post(
-  "/register",
+  "/login",
   [
-    check("firstName", "First Name is required").isString(),
-    check("lastName", "Last Name is required").isString(),
     check("email", "Email is required").isEmail(),
     check("password", "Password with 6 or more characters required").isLength({
       min: 6,
@@ -21,22 +19,23 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ message: errors.array() });
     }
-    try {
-      let user = await User.findOne({
-        email: req.body.email,
-      });
 
-      if (user) {
-        return res.status(400).json({
-          message: "User already exists",
-        });
+    const { email, password } = req.body;
+
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ message: "Invalid credentials" });
       }
 
-      user = new User(req.body);
-      await user.save();
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ message: "Invalid credentials" });
+      }
 
       const token = jwt.sign(
-        { userID: user.id },
+        { userId: user.id },
         process.env.JWT_SECRET_KEY as string,
         {
           expiresIn: "1d",
@@ -49,14 +48,10 @@ router.post(
         maxAge: 86400000,
       });
 
-      return res
-        .sendStatus(200)
-        .send({ message: "User registered successfully" });
+      res.status(200).json({ userId: user._id });
     } catch (error) {
       console.log(error);
-      res.status(500).send({
-        message: "Something went wrong",
-      });
+      return res.status(500).json({ message: "Something went wrong" });
     }
   }
 );
